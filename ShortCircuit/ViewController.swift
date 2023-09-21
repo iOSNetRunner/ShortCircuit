50200ea0bd79b1eafcb6b8f973ba7c6303d9131d
NetRunner <galatskiy@gmail.com>
2023-09-21 12:57:35 +0300

    Initial Commit

---
+++ ShortCircuit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShortCircuit
+//
+//  Created by Dmitrii Galatskii on 21.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
