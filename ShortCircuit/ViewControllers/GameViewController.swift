eb3858bbe5f1bda01753039add943f41319a6c60
NetRunner <galatskiy@gmail.com>
2023-09-24 01:21:40 +0300

    Main menu background animation ready. GameViewController: additional animations added. More accurate score updating. Dismiss added.

--- ShortCircuit/ViewControllers/GameViewController.swift
+++ ShortCircuit/ViewControllers/GameViewController.swift
@@ -7,9 +7,7 @@
 
 import UIKit
 
-
 final class GameViewController: UIViewController {
-    
     
     @IBOutlet var lifeIcon: UIImageView!
     @IBOutlet var backgroundImage: UIImageView!
@@ -21,21 +19,21 @@ final class GameViewController: UIViewController {
     @IBOutlet var buttonsBackground: UIView!
     @IBOutlet var gradientTintOverlay: UIView!
     
+    @IBOutlet var leftButton: UIButton!
+    @IBOutlet var rightButton: UIButton!
     
     @IBOutlet var playerXposition: NSLayoutConstraint!
-    
     
     @IBOutlet var scoreLabel: UILabel!
     @IBOutlet var lifeLeftLabel: UILabel!
     
     private let moveStep: CGFloat = 100
     
-    
     private var obstaclesPassed = 0
     
-    private var gameSpeed = 3.0
+    private var gameSpeed = 2.0
     private var playerLife = 3
-
+    
     private var spawnTimer: Timer?
     private var scoreTimer: Timer?
     private var spawnCoordinates: [CGFloat]?
@@ -54,13 +52,6 @@ final class GameViewController: UIViewController {
         
         spawnCoordinates = getSpawnCoordinates()
         
-        
-       
-        
-        
-    
-        
-        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -70,60 +61,49 @@ final class GameViewController: UIViewController {
         
         spawnTimer = Timer.scheduledTimer(timeInterval: gameSpeed / 3, target: self, selector: #selector(generateObstacles), userInfo: nil, repeats: true)
         
+        scoreTimer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateScore), userInfo: nil, repeats: true)
         
+        animatePlayerIdle()
     }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        scoreTimer = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(updateScore), userInfo: nil, repeats: true)
-        
-    }
-    
     
     
     
     @IBAction func exitButtonTapped(_ sender: Any) {
+        dismiss(animated: true)
     }
     
-    
     @IBAction func leftButtonPressed(_ sender: Any) {
-        
         if playerView.center.x - moveStep > view.bounds.minX + moveStep / 2 {
             playerXposition.constant -= moveStep
-            let animator = UIViewPropertyAnimator(duration: 0.2, curve: .linear)
+            
+            let animator = UIViewPropertyAnimator(duration: 0.1, curve: .linear)
             
             animator.addAnimations {
                 self.playerView.frame = self.playerView.frame.offsetBy(dx: -self.moveStep, dy: 0)
+                self.playerView.transform = CGAffineTransform(scaleX: 3, y: 0.1)
             }
-            
-            
+            animator.addCompletion { _ in
+                self.animatePlayerIdle()
+            }
             animator.startAnimation()
         }
-        
-        
-        
     }
-    
     
     @IBAction func rightButtonPressed(_ sender: Any) {
         if playerView.center.x + moveStep < view.bounds.maxX - moveStep / 2 {
             playerXposition.constant += moveStep
-            UIView.animate(withDuration: 0.2,
-                           delay: 0,
-                           usingSpringWithDamping: 2,
-                           initialSpringVelocity: 2,
-                           options: .curveLinear,
-                           animations: {
-                self.playerView.frame = self.playerView.frame.offsetBy(dx: self.moveStep, dy: 0)})
+            
+            let animator = UIViewPropertyAnimator(duration: 0.1, curve: .linear)
+            animator.addAnimations {
+                self.playerView.frame = self.playerView.frame.offsetBy(dx: self.moveStep, dy: 0)
+                self.playerView.transform = CGAffineTransform(scaleX: 3, y: 0.1)
+            }
+            animator.addCompletion { _ in
+                self.animatePlayerIdle()
+            }
+            animator.startAnimation()
         }
-        
-        
     }
-    
-    
-    
-    
     
     
     private func setVisualSettings() {
@@ -133,6 +113,7 @@ final class GameViewController: UIViewController {
         buttonsBackground.layer.borderWidth = 3
         buttonsBackground.layer.borderColor = UIColor.systemGreen.cgColor
         
+        lifeIcon.layer.shadowOpacity = 1
         lifeIcon.layer.shadowColor = UIColor.black.cgColor
         lifeIcon.layer.shadowRadius = 15
         
@@ -140,32 +121,28 @@ final class GameViewController: UIViewController {
         scoreBackground.layer.cornerRadius = buttonsBackground.layer.cornerRadius
         scoreBackground.layer.borderWidth = buttonsBackground.layer.borderWidth
         scoreBackground.layer.borderColor = buttonsBackground.layer.borderColor
+        
+        playerView.layer.shadowOpacity = 1
+        playerView.layer.shadowColor = UIColor.systemYellow.cgColor
+        playerView.layer.shadowRadius = 25
+        
     }
     
     private func animateBackground() {
-       
-            UIView.animate(withDuration: gameSpeed,
-                           delay: 0,
-                           options: [.repeat, .curveLinear],
-                           animations: {
-                self.backgroundImage.transform = CGAffineTransform(translationX: 0, y: self.view.bounds.maxY)
-                self.backgroundImageMirrored.transform = CGAffineTransform(translationX: 0, y: self.view.bounds.maxY)
-            }
-                
-            
-            )
-        
-        
+        UIView.animate(withDuration: gameSpeed,
+                       delay: 0,
+                       options: [.repeat, .curveLinear],
+                       animations: {
+            self.backgroundImage.transform = CGAffineTransform(translationX: 0, y: self.view.bounds.maxY)
+            self.backgroundImageMirrored.transform = CGAffineTransform(translationX: 0, y: self.view.bounds.maxY)
+        })
     }
-    
-    
     
     private func getSpawnCoordinates() -> [CGFloat] {
         let obstacleCoordinates = [view.center.x, view.center.x - moveStep, view.center.x + moveStep]
         
         return obstacleCoordinates
     }
-    
     
     private func lifeLeftCount() {
         if playerLife != 0 {
@@ -180,20 +157,53 @@ final class GameViewController: UIViewController {
         scoreLabel.text = "SCORE: \(obstaclesPassed)"
     }
     
+    private func animatePlayerIdle() {
+        UIView.animate(withDuration: 0.3,
+                       delay: 0,
+                       options: [.repeat],
+                       animations: {
+            self.playerView.transform = CGAffineTransform(scaleX: 1.1, y: 0.9)
+            self.playerView.transform = CGAffineTransform(scaleX: 0.9, y: 1.1)
+        })
+    }
+    
+    private func animatePlayerDeath() {
+        UIView.animate(withDuration: 0.5,
+                       delay: 0,
+                       options: [.curveLinear],
+                       animations: {
+            self.playerView.transform = CGAffineTransform(scaleX: 25, y: 0.2)
+        }, completion: { _ in
+            
+            UIView.animate(withDuration: 0.5,
+                           delay: 0,
+                           options: [.curveLinear],
+                           animations: {
+                self.playerView.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+            }, completion: { _ in
+                
+                self.playerView.removeFromSuperview()
+            })
+        })
+    }
+    
     @objc private func generateObstacles() {
         guard let spawnCoordinates else { return }
         guard let randomX = spawnCoordinates.randomElement() else { return }
         
         let frameSide: CGFloat = 60
         
-        let rectangle = UIImageView(frame: CGRect(x: randomX - frameSide / 2,
+        let obstacle = UIImageView(frame: CGRect(x: randomX - frameSide / 2,
                                                   y: view.bounds.minY,
                                                   width: frameSide,
                                                   height: frameSide))
-        rectangle.image = UIImage(systemName: "bolt.batteryblock.fill")
-        rectangle.tintColor = .orange
+        obstacle.image = UIImage(systemName: "bolt.batteryblock.fill")
+        obstacle.tintColor = .systemYellow
+        obstacle.layer.shadowOpacity = 1
+        obstacle.layer.shadowColor = obstacle.tintColor.cgColor
+        obstacle.layer.shadowRadius = 3
         
-        gradientTintOverlay.addSubview(rectangle)
+        gradientTintOverlay.addSubview(obstacle)
         
         
         let startAnimator = UIViewPropertyAnimator(duration: gameSpeed * 0.7, curve: .linear)
@@ -201,110 +211,41 @@ final class GameViewController: UIViewController {
         
         
         startAnimator.addAnimations {
-            
-            
-            rectangle.frame = rectangle.frame.offsetBy(dx: 0, dy: self.playerView.center.y)
-        
-            
+            obstacle.frame = obstacle.frame.offsetBy(dx: 0, dy: self.playerView.center.y)
         }
         
         endAnimator.addAnimations {
-            rectangle.frame = rectangle.frame.offsetBy(dx: 0, dy: self.view.bounds.maxY)
+            obstacle.frame = obstacle.frame.offsetBy(dx: 0, dy: self.view.bounds.maxY)
         }
         
         endAnimator.addCompletion { _ in
             
             self.updateScore()
-            rectangle.removeFromSuperview()
+            obstacle.removeFromSuperview()
+            
         }
         
         startAnimator.addCompletion { _ in
-            if rectangle.frame.intersects(self.playerView.frame) {
-            
-                                        if self.playerLife < 2 {
-                                            rectangle.removeFromSuperview()
-                                            self.spawnTimer?.invalidate()
-                                            startAnimator.stopAnimation(true)
-                                            self.view.subviews.forEach({$0.layer.removeAllAnimations()})
-                                            
-                                        }
-
-
-
-                                        self.lifeLeftCount()
-                                        self.updateLifeCounter()
-                                        rectangle.removeFromSuperview()
+            if obstacle.frame.intersects(self.playerView.frame) && self.playerLife != 0 {
                 
-                
-            
-            } else { self.obstaclesPassed += 1 }
-            
+                if self.playerLife < 2 {
+                    obstacle.removeFromSuperview()
+                    startAnimator.stopAnimation(true)
+                    self.spawnTimer?.invalidate()
+                    self.leftButton.isEnabled.toggle()
+                    self.rightButton.isEnabled.toggle()
+                    self.animatePlayerDeath()
+                    self.view.subviews.forEach({$0.layer.removeAllAnimations()})
+                    
+                }
+                self.lifeLeftCount()
+                self.updateLifeCounter()
+                obstacle.removeFromSuperview()
+            } else if self.playerLife != 0 {
+                self.obstaclesPassed += 1 }
             
             endAnimator.startAnimation()
         }
-        
-        
-        
         startAnimator.startAnimation()
-        print("LIFE \(playerLife) |   SCORE \(obstaclesPassed)")
-        
-        
-        
-//        UIView.animate(withDuration: gameSpeed * 0.7,
-//                           delay: 0,
-//                       options: [ .curveLinear],
-//                           animations: {
-//            rectangle.center.y += self.playerView.center.y
-//
-//
-//        }, completion: { _ in
-//
-//                    UIView.animate(withDuration: self.gameSpeed,
-//                                   delay: 0,
-//                                   options: [ .curveLinear],
-//                                   animations: {
-//                        if rectangle.frame.intersects(self.playerView.frame) {
-//
-//                            if self.playerLife < 2 {
-//                                self.spawnTimer?.invalidate()
-//                                self.view.subviews.forEach({$0.layer.removeAllAnimations()})
-//                            }
-//
-//
-//
-//                            self.lifeLeftCount()
-//                            self.updateLifeCounter()
-//                            rectangle.removeFromSuperview()
-//
-//
-//                        }
-//                        rectangle.center.y += self.view.bounds.maxY}, completion: { _ in
-//                            if rectangle.center.y > self.playerView.center.y - 500 {
-//                                self.obstaclesPassed += 1
-//                                self.updateScore()
-//                                print(self.obstaclesPassed)
-//                            }
-//                            rectangle.removeFromSuperview()
-//
-//                        }
-//
-//
-//
-//                    )
-//
-//
-//
-//
-//
-//                    }
-//                )
-            
-        
-        
-        
-        
     }
-    
-    
 }
-
--- ShortCircuit/ViewControllers/MainViewController.swift
+++ ShortCircuit/ViewControllers/MainViewController.swift
@@ -13,7 +13,9 @@ final class MainViewController: UIViewController {
     @IBOutlet var arrowsImage: UIImageView!
     @IBOutlet var gearLogo: UIImageView!
     @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var boltLogo: UIImageView!
     
+    private var spawnTimer: Timer?
     
     
     
@@ -25,13 +27,67 @@ final class MainViewController: UIViewController {
         view.setGradientBackground()
         
         
-        animateTitle()
-        animateArrows()
-        animateBackgroundGear()
+        
+        
+        
+        
         
     }
     
-  
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        spawnTimer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector( animateBackground), userInfo: nil, repeats: true)
+        
+        animateBackground()
+        animateTitle()
+        animateArrows()
+        animateBackgroundGear()
+        animateBolt()
+        
+        
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        titleLabel.transform = CGAffineTransform.identity
+        boltLogo.transform = CGAffineTransform.identity
+    }
+    
+    
+    
+    
+    
+    @objc private func animateBackground() {
+        let spawnPoint = CGFloat.random(in: view.bounds.minX...view.bounds.maxX)
+        let frameSide = CGFloat.random(in: 10...50)
+        
+        let bolt = UIImageView(frame: CGRect(x: spawnPoint,
+                                                  y: view.bounds.minY,
+                                                  width: frameSide,
+                                                  height: frameSide))
+        bolt.image = UIImage(systemName: "bolt.fill")
+        bolt.tintColor = .systemGreen
+        
+        bolt.layer.shadowColor = bolt.tintColor.cgColor
+        bolt.layer.shadowOpacity = 1
+        bolt.layer.shadowRadius = 10
+        
+        view.insertSubview(bolt, at: .max)
+        
+        UIView.animate(withDuration: 5,
+                       delay: 0,
+                       usingSpringWithDamping: 3.1,
+                       initialSpringVelocity: 0.1,
+                       options: [ .curveEaseInOut],
+                       animations: {
+            bolt.frame = bolt.frame.offsetBy(dx: spawnPoint, dy: self.view.bounds.maxY)
+            bolt.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+            bolt.alpha = Double.random(in: 0.2...1)
+        }, completion: { _ in
+            bolt.removeFromSuperview()
+        })
+    }
+    
     private func animateTitle() {
         UIView.animate(withDuration: 0.5,
                        delay: 0,
@@ -40,6 +96,23 @@ final class MainViewController: UIViewController {
                        options: [.repeat, .autoreverse],
                        animations: {
             self.titleLabel.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+            
+        })
+    }
+    
+    private func animateBolt() {
+        self.boltLogo.alpha = 0.05
+        UIView.animate(withDuration: 1,
+                       delay: 0,
+                       usingSpringWithDamping: 0.1,
+                       initialSpringVelocity: 5.2,
+                       options: [.repeat, .curveLinear],
+                       animations: {
+            
+            self.boltLogo.transform = CGAffineTransform(scaleX: 0.7, y: 0.7)
+            
+            
+            self.boltLogo.alpha = Double.random(in: 0.2...1)
         })
     }
     
@@ -57,15 +130,14 @@ final class MainViewController: UIViewController {
     }
     
     private func animateBackgroundGear() {
-        UIView.animate(withDuration: 5.0,
+        UIView.animate(withDuration: 3.0,
                        delay: 0,
                        
                        options: [.repeat, .curveLinear],
                        animations: {
-            self.gearLogo.transform = self.gearLogo.transform.rotated(by: 2.09)
-            
+            self.gearLogo.transform = CGAffineTransformMakeRotation(.pi * 0.66)
+            self.gearLogo.transform = CGAffineTransformMakeRotation(0)
         })
-        
     }
     
     
